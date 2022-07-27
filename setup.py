9728c585f3c905491b5beef7144f2cfd962f8496
Ranadeep Biswas <mail@rnbguy.at>
2022-07-27 12:29:42 +0200

    pylama[all]

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='pylama-pre-commit',
     description='Pylama pre-commit support',
     url='https://github.com/gvanderest/pylama-pre-commit',
-    version='0.1.0',
+    version='0.2.0',
     author='Guillaume VanderEst',
     author_email='gvanderest@gmail.com',
     install_requires=[
-        'pylama',
+        'pylama[all]',
     ],
 )
